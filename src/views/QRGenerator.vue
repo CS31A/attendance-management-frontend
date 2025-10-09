f22091c926852ac1e8456af87878b5345c13327a
Edrian <edrianmangubat444@gmail.com>
2025-10-09 23:59:07 +0800

    added a teacher form for the ui

--- src/views/QRGenerator.vue
+++ src/views/QRGenerator.vue
@@ -125,52 +125,132 @@ export default {
       </h1>
       <p class="app-subtitle">Assign subjects to teachers and generate QR codes</p>
     </header>
+      <!-- Teachers Management Section -->
+    <section class="teachers-section">
+      <div class="section-header">
+        <h2 class="section-title">
+          <svg class="section-icon" viewBox="0 0 24 24" fill="currentColor">
+            <path d="M16 11c1.66 0 2.99-1.34 2.99-3S17.66 5 16 5c-1.66 0-3 1.34-3 3s1.34 3 3 3zm-8 0c1.66 0 2.99-1.34 2.99-3S9.66 5 8 5C6.34 5 5 6.34 5 8s1.34 3 3 3zm0 2c-2.33 0-7 1.17-7 3.5V19h14v-2.5c0-2.33-4.67-3.5-7-3.5zm8 0c-.29 0-.62.02-.97.05 1.16.84 1.97 1.97 1.97 3.45V19h6v-2.5c0-2.33-4.67-3.5-7-3.5z"/>
+          </svg>
+          Teachers ({{ teachers.length }})
+        </h2>
+        <button @click="toggleTeacherForm" class="add-teacher-btn">
+          <svg class="btn-icon" viewBox="0 0 24 24" fill="currentColor">
+            <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/>
+          </svg>
+          {{ showTeacherForm ? 'Cancel' : 'Add Teacher' }}
+        </button>
+      </div>
 
-    <!-- Subject Form -->
-    <section class="form-section">
-      <div class="subject-form">
-        <h3 class="form-title">Add New Subject</h3>
-        <div class="form-grid">
-          <div class="input-group">
-            <label class="input-label">Subject Name</label>
-            <input
-              v-model="newSubject.name"
-              type="text"
-              placeholder="Enter subject name"
-              class="form-input"
-              @keyup.enter="addSubject"
-            />
-          </div>
-          <div class="input-group">
-            <label class="input-label">Subject Code</label>
-            <input
-              v-model="newSubject.code"
-              type="text"
-              placeholder="Enter subject code"
-              class="form-input"
-              @keyup.enter="addSubject"
-            />
-          </div>
+      <!-- Add Teacher Form -->
+      <div v-if="showTeacherForm" class="teacher-form">
+        <div class="form-grid-2">
           <div class="input-group">
             <label class="input-label">Teacher Name</label>
             <input
-              v-model="newSubject.teacher"
+              v-model="newTeacher.name"
               type="text"
               placeholder="Enter teacher name"
               class="form-input"
-              @keyup.enter="addSubject"
+              @keyup.enter="addTeacher"
+            />
+          </div>
+          <div class="input-group">
+            <label class="input-label">Email Address</label>
+            <input
+              v-model="newTeacher.email"
+              type="email"
+              placeholder="Enter email address"
+              class="form-input"
+              @keyup.enter="addTeacher"
             />
           </div>
           <div class="input-group">
             <button 
-              @click="addSubject"
-              :disabled="!canAddSubject"
+              @click="addTeacher"
+              :disabled="!canAddTeacher"
+              class="submit-btn"
+            >
+              <svg class="btn-icon" viewBox="0 0 24 24" fill="currentColor">
+                <path d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41z"/>
+              </svg>
+              Add Teacher
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <!-- Teachers List -->
+      <div class="teachers-grid">
+        <div v-for="teacher in teachers" :key="teacher.id" class="teacher-card">
+          <div class="teacher-avatar">
+            {{ teacher.name.charAt(0) }}
+          </div>
+          <div class="teacher-info">
+            <h4 class="teacher-name">{{ teacher.name }}</h4>
+            <p class="teacher-email">{{ teacher.email }}</p>
+          </div>
+          <button @click="removeTeacher(teacher.id)" class="remove-teacher-btn" title="Remove Teacher">
+            <svg viewBox="0 0 24 24" fill="currentColor">
+              <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
+            </svg>
+          </button>
+        </div>
+      </div>
+    </section>
+
+    <!-- Subject Assignment Form -->
+    <section class="form-section">
+      <div class="subject-form">
+        <h3 class="form-title">
+          <svg class="section-icon" viewBox="0 0 24 24" fill="currentColor">
+            <path d="M19 3h-4.18C14.4 1.84 13.3 1 12 1c-1.3 0-2.4.84-2.82 2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-7 0c.55 0 1 .45 1 1s-.45 1-1 1-1-.45-1-1 .45-1 1-1zm2 14H7v-2h7v2zm3-4H7v-2h10v2zm0-4H7V7h10v2z"/>
+          </svg>
+          Assign Subject to Teacher
+        </h3>
+        <div class="form-grid">
+          <div class="input-group">
+            <label class="input-label">Select Subject</label>
+            <select
+              v-model="newAssignment.subjectId"
+              class="form-select"
+            >
+              <option value="" disabled>Choose a subject...</option>
+              <option 
+                v-for="subject in unassignedSubjects" 
+                :key="subject.id" 
+                :value="subject.id"
+              >
+                {{ subject.code }} - {{ subject.name }}
+              </option>
+            </select>
+          </div>
+          <div class="input-group">
+            <label class="input-label">Select Teacher</label>
+            <select
+              v-model="newAssignment.teacherId"
+              class="form-select"
+            >
+              <option value="" disabled>Choose a teacher...</option>
+              <option 
+                v-for="teacher in teachers" 
+                :key="teacher.id" 
+                :value="teacher.id"
+              >
+                {{ teacher.name }}
+              </option>
+            </select>
+          </div>
+          <div class="input-group">
+            <button 
+              @click="assignSubject"
+              :disabled="!canAssignSubject"
               class="submit-btn"
             >
               <svg class="btn-icon" viewBox="0 0 24 24" fill="currentColor">
                 <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/>
               </svg>
-              Add Subject
+              Assign & Generate QR
             </button>
           </div>
         </div>
