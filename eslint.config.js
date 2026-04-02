61a542c16a5182591bb2f3853d6970d356c50530
Neil Vallecer <neilvallecer12@gmail.com>
2026-04-02 23:36:53 +0800

    refactor: enable strict TyepScript checking and disallow explicit any

--- eslint.config.js
+++ eslint.config.js
@@ -7,6 +7,7 @@ export default antfu({
     quotes: 'single',
   },
   ignores: [
+    '.serena/**',
     '**/*.md',
     '**/dist/**',
     '**/node_modules/**',
@@ -16,5 +17,6 @@ export default antfu({
     'no-console': ['error', { allow: ['warn', 'error'] }],
     'regexp/no-super-linear-backtracking': 'off',
     '@typescript-eslint/ban-ts-comment': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
   },
 })
